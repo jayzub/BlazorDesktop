8592b93e72140d7e7a19728568bf71213408dd32
Jayzub <jamiu_zubair@yahoo.com>
2022-10-24 10:47:43 -0400

    Add project files.

---
+++ src/WebviewAppShared/Data/DeviceModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebviewAppShared.Data
+{
+    internal class DeviceModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public string Serial_number { get; set; }
+
+        public string Status { get; set; }
+
+        public DateTime Last_tested { get; set; }
+    }
+}
