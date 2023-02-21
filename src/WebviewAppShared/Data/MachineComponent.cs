9861eb643af565dd752d8f849ee0529713afe0a6
Mir Ashraf Ali <49491752+ashraf7ali@users.noreply.github.com>
2023-02-21 10:25:00 -0500

    Add files via upload

---
+++ src/WebviewAppShared/Data/MachineComponent.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebviewAppShared.Data
+{
+    public class MachineComponent
+    {
+        public string Id { get; set; }
+        public string Machine_Name { get; set; }
+        public string Axis { get; set; }
+        public string Component { get; set; }
+
+        public int count { get; set; }
+
+    }
+}
