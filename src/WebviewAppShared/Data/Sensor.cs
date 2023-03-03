e3de85ed578de2d543104c3d0de4269d54712b8a
Ashraf Ali <ashraf.ali771@gmail.com>
2023-03-02 22:55:59 -0500

    Added Sensor Crud in settings page.

---
+++ src/WebviewAppShared/Data/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebviewAppShared.Data
+{
+    public class Sensor
+    {
+        public string name { get; set; }
+        public int min { get; set; }
+        public int max { get; set; }
+    }
+}
