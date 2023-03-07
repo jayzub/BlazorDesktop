f47ef4f69198b401eb8dafba0184e00f97868d65
KashyapKapadia <kashyapkapadia3487@gmail.com>
2023-03-06 19:12:18 -0500

    Added drop down for Machine name and sensor name

---
+++ src/WebviewAppShared/Data/MachineModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebviewAppShared.Data
+{
+    public class MachineModel
+    {
+        public int Id { get; set; }
+        public string Machine_Name { get; set; }
+    }
+}
