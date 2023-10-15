d5f4661dbf8797cebad9c1b8c281d8152ada7999
Ashraf Ali <ashraf.ali771@gmail.com>
2023-10-15 14:22:26 -0400

    updated error handling and graph axis

--- src/WebviewAppShared/Data/S.cs
+++ src/WebviewAppShared/Data/S.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,11 +13,12 @@ namespace WebviewAppShared.Data
         public string S2 { get; set; } //name cDAQ1Mod1
         public string S3 { get; set; } //DAQ Model
         public long S4 { get; set; } //DAQ Serial 
+        [Required(ErrorMessage = "Accelerometer value required.")]
         public string S5 { get; set; } //Accelerometer
-        public double S6 { get; set; } // Accel X Sens
-        public double S7 { get; set; } // Accel Y Sens
-        public double S8 { get; set; } // Accel Z Sens
-        public int S9 { get; set; } // Sensor sampling frequnecy
+        public double S6 { get; set; } = 100; // Accel X Sens 
+        public double S7 { get; set; } = 100.2;// Accel Y Sens
+        public double S8 { get; set; } = 100.4;// Accel Z Sens
+        public int S9 { get; set; } = 12800;// Sensor sampling frequnecy
 
     }
 }
--- src/WebviewAppShared/Data/TD.cs
+++ src/WebviewAppShared/Data/TD.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -9,10 +10,16 @@ namespace WebviewAppShared.Data
     public class TD
     {
         public int TD1 { get; set; }
+        [Required(ErrorMessage = "Machine is required.")]
         public string TD2 { get; set; }
+        [Required(ErrorMessage = "Hours is required.")]
         public string TD3 { get; set; }
+        [Required(ErrorMessage = "Operator is required.")]
         public string TD4 { get; set; }
+
         public string TD5 { get; set; }
+
+        [Required(ErrorMessage = "Sensor is required.")]
         public string TD6 { get; set; }
         public DateTime TD7 { get; set; }
         public string TD8 { get; set; }
