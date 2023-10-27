eb420997d96ba59bbc37d31d443e721deaf88833
Ashraf Ali <ashraf.ali771@gmail.com>
2023-10-26 21:15:27 -0400

    view partial collected data

--- src/WebviewAppShared/Data/S.cs
+++ src/WebviewAppShared/Data/S.cs
@@ -13,8 +13,8 @@ namespace WebviewAppShared.Data
         public string S2 { get; set; } //name cDAQ1Mod1
         public string S3 { get; set; } //DAQ Model
         public long S4 { get; set; } //DAQ Serial 
-        [Required(ErrorMessage = "Accelerometer value required.")]
-        public string S5 { get; set; } //Accelerometer
+
+        public string S5 { get; set; }  //Accelerometer 
         public double S6 { get; set; } = 100; // Accel X Sens 
         public double S7 { get; set; } = 100.2;// Accel Y Sens
         public double S8 { get; set; } = 100.4;// Accel Z Sens
